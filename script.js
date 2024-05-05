b8f1a65b450b3d4eb5a1649c7b840a0d0de0b626
Shadab Mahtabi <shadabmanpuri@gmail.com>
2024-05-05 23:57:34 +0530

    showing data in the form

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 let get_location = document.querySelector(".current_location");
+let inputs = document.querySelectorAll('form > input')
 
 get_location.addEventListener("click", (e) => {
   navigator.geolocation.getCurrentPosition(
     async (data) => {
       let { latitude, longitude } = data.coords;
-      console.log({ latitude, longitude });
+    //   console.log({ latitude, longitude });
       let location = await axios.get(
         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
       );
 
       let { locality, city, principalSubdivision, countryName } = location.data;
-      let address = await JSON.stringify({
-        locality,
-        city,
-        principalSubdivision,
-        countryName,
-      });
-      document.querySelector("#address").innerHTML = address;
-      console.log(location);
+      inputs[0].value = locality;
+      inputs[1].value = city;
+      inputs[2].value = principalSubdivision;
+      inputs[3].value = countryName;
+      console.log(location.data);
       // 24.819466, 85.028421
     },
     (err) => console.log(err.message)
